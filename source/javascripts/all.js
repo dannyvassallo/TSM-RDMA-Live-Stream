170793a9f76cad2d63c2f7468a0da0c90cdce150
mqschwanda <mqschwanda@gmail.com>
2018-04-27 11:25:26 -0400

    update video link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,53 @@
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require_tree .
+
+var $iframe;
+var $videoScreen;
+var $clickHere;
+
+var isReady = false;
+var youtubeId = '5wTty7vUA9I';
+
+function handleReady() {
+  $iframe = $('iframe');
+  $videoScreen = $('#video-screen');
+  $clickHere = $('#click-here');
+
+  isReady = true;
+}
+
+function handleResize() {
+  if (isReady) {
+    var width = $videoScreen.width();
+    var height = width * 9 / 16;
+
+    $iframe.width(width).height(height);
+  }
+}
+
+function handleClick() {
+  var url = 'https://www.youtube.com/embed/';
+  var qs = '?rel=0&showinfo=0&autoplay=1';
+  $iframe.prop('src', url + youtubeId + qs);
+
+  handleResize();
+
+  $iframe.show();
+  $videoScreen.hide();
+  $clickHere.hide();
+}
+
+// function handleClick() {
+//   var url = 'https://www.youtube.com/embed/';
+//   var qs = '?rel=0&showinfo=0&autoplay=1';
+//   window.open(url + youtubeId + qs);
+// }
+
+$(document).ready(handleReady);
+
+$(window).resize(function() {
+  if (isReady && $iframe.is(':visible')) handleResize();
+});
+
+$('#streaming-video').on('click', handleClick);
